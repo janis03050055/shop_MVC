bb250c39acd093ebad62e02d0608565705c8d7a2
janis <janis03050055@gmail.com>
2018-02-22 17:56:34 +0800

    新增一View可接收Controller資料

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -8,19 +8,21 @@ namespace MvcMovie.Controllers
         //HTTP 端點是 Web 應用程式中的可設定目標 URL 
         // 原網址後加上 /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            //預設action
-            return "This is my default action...";
+            //因為沒有明確指定檢視範本檔案的名稱，MVC 預設為使用 /Views/HelloWorld 資料夾中的 Index.cshtml 檢視檔案。 
+            return View();
         }
 
         // 
         // 原網址後加上 /HelloWorld/Welcome/ 
         // http://localhost:xxxx/HelloWorld/Welcome?name=Janis&numtimes=4
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            //使用action並傳回[Parameters]
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            //將資料從控制端傳送到View，記得要return view
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
