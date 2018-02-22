ae8c36bad93626630d81ad4e000b86c549291180
janis <janis03050055@gmail.com>
2018-02-22 17:15:25 +0800

    新增一controller傳URL

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //HTTP 端點是 Web 應用程式中的可設定目標 URL 
+        // 原網址後加上 /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // 原網址後加上 /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
